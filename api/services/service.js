8034a21ab0a0b7ee245fb85e083167fbed4ca890
DEMETRIXASUSgabri <gabriele.percoco@demetrix.it>
2026-05-22 11:33:55 +0200

    clear log

--- api/services/service.js
+++ api/services/service.js
@@ -89,8 +89,10 @@ async function executeRequest(req, res) {
       mustDownload = true
       logger.info(`Entity ${id} not found in database, mapping...`)
     }
-    if (!mustDownload && !mustUpdate)
+    if (!mustDownload && !mustUpdate){
+      logger.info(`Entity ${id} is up to date, skipping...`)
       continue
+    }
     let mapID =
       req.query.mapID || req.params.mapID || ent.mapID || config.mapID;
 
@@ -179,7 +181,8 @@ async function executeRequest(req, res) {
             let purged = false
             for (let chunkIndex = 0; (response.data[0] || response.data.id); chunkIndex++) { // Loop per gestire i chunk
               //while (response.data[0] || response.data.id) {
-              logger.info(response.data.status)
+              logger.info(response.data.status || response.status)
+              logger.info(`Fetching chunk ${chunkIndex} for outputId ${outputId}`);
               response = await axios.get((config.sessionEndpoint || "http://localhost:5500/api/output?") + "id=" + outputId + "&lastId=" + lastId + "&index=" + chunkIndex, {
                 headers: {
                   Authorization: `Bearer ${bearerToken}`
